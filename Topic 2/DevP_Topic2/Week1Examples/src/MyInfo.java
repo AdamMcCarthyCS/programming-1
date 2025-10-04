ed0486679601252ee2cecd4d8371b618e5ede97f
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-04 11:04:31 +0100

    feat(exercise-2): Use the int, double and char types
    - initialise int, double, and char variables and print them

---
+++ Topic 2/DevP_Topic2/Week1Examples/src/MyInfo.java
@@ -0,0 +1,11 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        char firstNameInitial = 'A';
+        int myAge = 37;
+        double myHeight = 1.79;
+
+        System.out.println("My initial is " + firstNameInitial +
+                ", my age is " + myAge + ", my height is " + myHeight);
+
+    }
+}
