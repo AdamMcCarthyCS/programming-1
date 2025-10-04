11ed950d22f4159e3e60fa8f0019ebfa66eee3b0
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-04 11:29:43 +0100

    feat(exercise-2): Write a program that demonstrates integer and double division
    - declare one variable to store the result of dividing two integers
    - declare another variable to cast one one the integers to a double to demonstrate how double results are generated

---
+++ Topic 2/DevP_Topic2/Week1Examples/src/IntDoubleDivision.java
@@ -0,0 +1,12 @@
+public class IntDoubleDivision {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+
+        int result1 = a / b;
+        double result2 = (double) a / b;
+
+        System.out.println("int division: " + result1);
+        System.out.println("double division " + result2);
+    }
+}
