cc1591612029dc8aa708b7a6e1c00eadc7bc6083
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-08 15:43:16 +0100

    feat(console-exercise-3): print launch countdown using loops

---
+++ 2b - Iteration (Loops)/Dev2b/Week2Class2Examples/src/ConsoleExercise3.java
@@ -0,0 +1,19 @@
+public class ConsoleExercise3 {
+    public static void main(String[] args) {
+
+        /*
+        int i = 10;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+        System.out.println("blast off!");
+         */
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("blast off!");
+    }
+}
