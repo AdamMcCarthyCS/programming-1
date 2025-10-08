8f373a0e1ce784e106124e0da8749848f2db8722
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-08 15:02:20 +0100

    feat(exercise2): Use while loop to print ints 1 to 10

--- 2b - Iteration (Loops)/Dev2b/Week2Class2Examples/src/CountTo10.java
+++ 2b - Iteration (Loops)/Dev2b/Week2Class2Examples/src/CountTo10.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class CountTo10 {
+    public static void main(String[] args) {
+        // declare counter variable
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            // update counter
+            i++;
+        }
+    }
 }
