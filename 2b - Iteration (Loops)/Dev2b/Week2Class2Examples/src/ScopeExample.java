64fe76e500f62e628094095960b794a9e8f94bc6
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-08 14:54:05 +0100

    fix(exercise1): refactor variable assignment scope

---
+++ 2b - Iteration (Loops)/Dev2b/Week2Class2Examples/src/ScopeExample.java
@@ -0,0 +1,14 @@
+public class ScopeExample {
+    public static void main(String[] args) {
+        int myAge = 21;
+        String details;
+
+        if (myAge >= 18) {
+            details = myAge + " years old - Get ready to vote";
+        } else {
+            details = myAge + " years old - Ah you cant vote yet";
+        }
+
+        System.out.println(details);
+    }
+}
