301af981707b17890ae12e948e5eede356006325
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-08 15:15:22 +0100

    feat(exercise2): Use a nested while loop to print an int series

--- 2b - Iteration (Loops)/Dev2b/Week2Class2Examples/src/NestedWhileLoop.java
+++ 2b - Iteration (Loops)/Dev2b/Week2Class2Examples/src/NestedWhileLoop.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class NestedWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 4) {
+            int j = 0;
+            while (j < 4) {
+                System.out.println("The value of i is " + i + " and j is " + j);
+                j++;
+            }
+            i++;
+        }
+    }
 }
