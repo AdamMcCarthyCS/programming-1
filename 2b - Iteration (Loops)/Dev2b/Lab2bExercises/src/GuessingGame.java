a0d0c23326682f59f09d21c0a2bed4f62e3176d6
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-09 19:16:55 +0100

    refactor(challenge2): Added functionality to track number of guesses

--- 2b - Iteration (Loops)/Dev2b/Lab2bExercises/src/GuessingGame.java
+++ 2b - Iteration (Loops)/Dev2b/Lab2bExercises/src/GuessingGame.java
@@ -6,7 +6,8 @@ public class GuessingGame {
         Scanner input = new Scanner(System.in);
         // store the secret number the user will guess
         int secretMeaningOfLife = 42;
-
+        // store number of guesses the user makes
+        int guessCounter = 0;
         // use a number outside the range to begin the loop
         int guess = -1;
 
@@ -17,13 +18,24 @@ public class GuessingGame {
             // give user a hint if they guessed incorrectly
             if (guess < secretMeaningOfLife) {
                 System.out.println("Too low!");
+                guessCounter++;
             }
 
             if (guess > secretMeaningOfLife) {
                 System.out.println("Too high!");
+                guessCounter++;
             }
         }
+        // Add extra guess for the correct guess
+        guessCounter++;
+
+        // Output message showing the program has finished
         System.out.println("Congratulations, you now know the meaning of life," +
                 " the universe, everything!");
+        if (guessCounter == 1) {
+            System.out.println("You only needed 1 guess! Amazing!");
+        } else {
+            System.out.println("It took you " + guessCounter + " guesses!");
+        }
     }
 }
