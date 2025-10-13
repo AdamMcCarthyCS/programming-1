67f962b3d4d298f5558d5294bfc24910f179e3a2
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-13 17:04:38 +0100

    feat(1-driver-class): Use Driver class to call a method from another class

---
+++ 3a - Methods/Dev/Lab 3a/Week3ClassExamples/src/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main(String[] args) {
+    MethodExamples example = new MethodExamples();
+    // Call a method with no parameters
+    example.sayHello();
+    }
+}
