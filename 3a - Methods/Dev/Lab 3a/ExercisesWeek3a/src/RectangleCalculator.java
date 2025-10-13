5b02458dd96f6230a54294d46c76f1d3c1ef94b6
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-13 19:02:35 +0100

    feat(exercise-2-extra): Overload methods to include use of Scanner

--- 3a - Methods/Dev/Lab 3a/ExercisesWeek3a/src/RectangleCalculator.java
+++ 3a - Methods/Dev/Lab 3a/ExercisesWeek3a/src/RectangleCalculator.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class RectangleCalculator {
+/*    use a single scanner instance for both methods
+I don't like this as it does not allow me to close the input stream */
+    private Scanner input = new Scanner(System.in);
 
     public void printArea(double width, double height) {
         double area = width * height;
@@ -8,5 +13,27 @@ public class RectangleCalculator {
     public void printPerimeter(double width, double height) {
         double perimeter = 2 * width + 2 * height;
         System.out.println("Perimeter: " + perimeter);
+    }
+
+    public void printArea() {
+        // read in the width and height to be used in the calculation
+        System.out.print("Enter the width of the rectangle: ");
+        double width = input.nextDouble();
+        System.out.print("Enter the height of the rectangle: ");
+        double height = input.nextDouble();
+
+        // use other implementation to carry out calculation logic
+        printArea(width, height);
+    }
+
+    public void printPerimeter() {
+        // read in the width and height to be used in the calculation
+        System.out.print("Enter the width of the rectangle: ");
+        double width = input.nextDouble();
+        System.out.print("Enter the height of the rectangle: ");
+        double height = input.nextDouble();
+
+        // use other implementation to carry out calculation logic
+        printPerimeter(width, height);
     }
 }
