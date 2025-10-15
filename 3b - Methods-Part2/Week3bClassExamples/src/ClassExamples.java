f3b81fe29652abe4fa52a2242800810d18d2ba05
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-15 14:24:46 +0100

    feat(example-1): Create a method to cube an integer
    - Created a method which cubes an integer given as an argument
    - Verified it works by calling it in driver class and printing the value out

--- 3b - Methods-Part2/Week3bClassExamples/src/ClassExamples.java
+++ 3b - Methods-Part2/Week3bClassExamples/src/ClassExamples.java
@@ -1,3 +1,9 @@
 public class ClassExamples {
 
+    /**
+     * A method to compute the cube of an integer
+     */
+    public int cubeOfInteger(int number) {
+        return number * number * number;
+    }
 }
--- 3b - Methods-Part2/Week3bClassExamples/src/Driver.java
+++ 3b - Methods-Part2/Week3bClassExamples/src/Driver.java
@@ -1,5 +1,12 @@
 public class Driver {
     public static void main(String[] args){
 
+        // create an instance of the ClassExamples class
+        ClassExamples example = new ClassExamples();
+
+        // cube the number and output the result
+        int numToBeCubed = 4;
+        int cubedNumber = example.cubeOfInteger(numToBeCubed);
+        System.out.println(numToBeCubed + " cubed is " + cubedNumber);
     }
 }
