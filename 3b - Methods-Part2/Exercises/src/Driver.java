c38407c00546120a7296e3c4a625ce800dc0e617
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-17 12:04:46 +0100

    feat(exercise-1): Add method to sum three integers
    - Create a method that reads in three integers using the Scanner class and then sums the integers and outputs to standard output
    - Call the method from the Driver class

--- 3b - Methods-Part2/Exercises/src/Driver.java
+++ 3b - Methods-Part2/Exercises/src/Driver.java
@@ -1,2 +1,18 @@
+/**
+ * The Driver class serves as the main entry point for the program.
+ *
+ * <p>The class contains the {@code main} method which starts the application</p>
+ *
+ * @author Adam McCarthy
+ * @version 1.0
+ */
 public class Driver {
+    public static void main(String[] args) {
+
+        // create an instance of the exercises class
+        Exercises exercise = new Exercises();
+
+        // Exercise 1
+        exercise.addNum();
+    }
 }
--- 3b - Methods-Part2/Exercises/src/Exercises.java
+++ 3b - Methods-Part2/Exercises/src/Exercises.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * A class to hold all exercises from the week 3b lab
+ *
+ * @author Adam McCarthy
+ * @version 1.0
+ */
 public class Exercises {
+    private Scanner scanner = new Scanner(System.in);
+
+    /**
+     *  A method which reads in three integers from the user with the Scanner class and then prints their
+     *  sum.
+     */
+    public void addNum() {
+        // prompt the user to enter the three integers for the sum
+        System.out.print("Enter the first integer: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.print("Enter the third integer: ");
+        int thirdNumber = scanner.nextInt();
+
+        // print the sum of the three integers to standard output in the required format
+        System.out.println(firstNumber + " + " + secondNumber + " + " + thirdNumber + " = "
+            + (firstNumber + secondNumber + thirdNumber));
+    }
 }
