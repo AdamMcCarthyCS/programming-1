8d42c68a796997d4af209b7269a99e745c2522df
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-17 14:06:46 +0100

    feat(challenge): Overload the futurePrediction method to accept string input
    - Used if else statements to determine output
    - Added a default value for incorrect input
    - Tested by printing result in the Driver class

--- 3b - Methods-Part2/ChallengeWeek3b/src/Driver.java
+++ 3b - Methods-Part2/ChallengeWeek3b/src/Driver.java
@@ -1,5 +1,23 @@
+/**
+ * This is the entry point to the program.
+ *
+ * <p>This class contains the {@code main} method.</p>
+ *
+ * @author Adam McCarthy
+ * @version 1.0
+ */
 public class Driver {
     public static void main(String[] args) {
 
+        // create a fortuneTeller object
+        FortuneTeller fortune = new FortuneTeller();
+
+        String messageOne = fortune.futurePrediction("aries");
+        String messageTwo = fortune.futurePrediction("pisces");
+        String messageThree = fortune.futurePrediction("orion");
+
+        System.out.println("Message 1: " + messageOne);
+        System.out.println("Message 2: " + messageTwo);
+        System.out.println("Message 3: " + messageThree);
     }
 }
--- 3b - Methods-Part2/ChallengeWeek3b/src/FortuneTeller.java
+++ 3b - Methods-Part2/ChallengeWeek3b/src/FortuneTeller.java
@@ -1,3 +1,10 @@
+/**
+ * This class contains methods for predicting the users fortune and provides extra functionality
+ * beyond the lab 3a version
+ *
+ * @author Adam McCarthy
+ * @version 2.0
+ */
 public class FortuneTeller {
 
     public void sayRandomFortune() {
@@ -13,5 +20,39 @@ public class FortuneTeller {
         return "Dont worry that you're " + age + " now. By the time you're " + (age + 10) +
                 ", anti-aging cream will have finally been perfected!";
 
+    }
+
+    public String futurePrediction(String starSign) {
+        String message;
+
+        if (starSign.equalsIgnoreCase("aries")) {
+            message = "You will receive a golden ram!";
+        } else if (starSign.equalsIgnoreCase("taurus")) {
+            message = "A lone bull will  bring you good luck!";
+        } else if (starSign.equalsIgnoreCase("gemini")) {
+            message = "Twin fortunes await you!";
+        } else if (starSign.equalsIgnoreCase("cancer")) {
+            message = "Your fate crawls by the seashore!";
+        } else if (starSign.equalsIgnoreCase("leo")) {
+            message = "You will be king of men and animals!";
+        } else if (starSign.equalsIgnoreCase("virgo")) {
+            message = "A maiden will bring you good luck!";
+        } else if (starSign.equalsIgnoreCase("libra")) {
+            message = "Your effort and rewards will weigh equally!";
+        } else if (starSign.equalsIgnoreCase("scorpio")) {
+            message = "You will be rewarded for crossing the sands!";
+        } else if (starSign.equalsIgnoreCase("sagittarius")) {
+            message = "You will be struck by fates arrow!";
+        } else if (starSign.equalsIgnoreCase("capricorn")) {
+            message = "You will be the GOAT in your chosen adventure!";
+        } else if (starSign.equalsIgnoreCase("aquarius")) {
+            message = "You will receive good fortune when the rain falls around you!";
+        } else if (starSign.equalsIgnoreCase("pisces")){
+            message = "Your best ideas will come when you step in the waters!";
+        } else {
+            message = starSign + " is not a valid star sign!";
+        }
+
+        return message;
     }
 }
