dfc98c9a04ec8016a98b8406ca30537dfcfac030
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-20 15:19:39 +0100

    feat(exercise-1): Read six integers and print to standard output
    - Add method that reads integers to array and then prints contents

--- 4a - Arrays/ArrayExercises/src/Driver.java
+++ 4a - Arrays/ArrayExercises/src/Driver.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        // create a ArrayClass instance
+        ArrayClass ac = new ArrayClass();
+        ac.exercise1();
     }
 }
