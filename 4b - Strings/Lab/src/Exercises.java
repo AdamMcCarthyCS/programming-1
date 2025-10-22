02043b65752c90f89f7ce7ae1bda2fefc7f4d7a0
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-22 17:23:29 +0100

    feat(exercise1): Use charAt method to print a character in a string

--- 4b - Strings/Lab/src/Exercises.java
+++ 4b - Strings/Lab/src/Exercises.java
@@ -1,3 +1,20 @@
 public class Exercises {
 
+    /**
+     * Demonstrate the use of the charAt string method
+     */
+    public void exer1(){
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String errorMessage404 = "HTTP 404 Not Found Error";
+
+        System.out.println("The character at position 4 in "
+            + alphabet
+            + " is "
+            + alphabet.charAt(3));
+
+        System.out.println("The character at position 10 in "
+            + errorMessage404
+            + " is "
+            + errorMessage404.charAt(9));
+    }
 }
