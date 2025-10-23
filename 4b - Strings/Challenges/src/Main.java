8198b6425fe86b0990d652a8f7c0095a991f9762
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-23 21:55:27 +0100

    feat(challenge2): Add program to count the number of vowels and consonants in a user entered sentence

--- 4b - Strings/Challenges/src/Main.java
+++ 4b - Strings/Challenges/src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        // create instance of the Pallindrome class
-        Palindrome palindrome = new Palindrome();
-        // run palindrome checker program
-        // Try "Do geese see god"
-        // Or, the Finnish word: saippuakivikauppias
-        // Or, "Dammit im mad"
-        palindrome.isPalindrome();
+//        // create instance of the Pallindrome class
+//        Palindrome palindrome = new Palindrome();
+//        // run palindrome checker program
+//        // try "Do geese see god"
+//        // or, the Finnish word: saippuakivikauppias
+//        // or, "Dammit im mad"
+//        palindrome.isPalindrome();
+//
+//        // skip a line in the output
+//        System.out.println();
 
+        // create an instance of the CountVowelsAndConsonants class
+        CountVowelsAndConsonants counter = new CountVowelsAndConsonants();
+        // try "Java Programming is fun!"
+        counter.countVowelAndConsonant();
     }
 }
