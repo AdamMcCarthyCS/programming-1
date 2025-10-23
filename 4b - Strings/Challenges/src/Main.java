d3cbed9cae6bb4f255d6af97c13ed594997cb572
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-10-23 20:39:08 +0100

    feat(challenge1): Add program to check if user enters a palindrome and test in Main

---
+++ 4b - Strings/Challenges/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // create instance of the Pallindrome class
+        Palindrome palindrome = new Palindrome();
+        // run palindrome checker program
+        // Try "Do geese see god"
+        // Or, the Finnish word: saippuakivikauppias
+        // Or, "Dammit im mad"
+        palindrome.isPalindrome();
+
+    }
+}
