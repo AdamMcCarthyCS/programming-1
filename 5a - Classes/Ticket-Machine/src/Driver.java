78e089dc47b02574c2fd319150b13f86df8d637f
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-03 19:20:13 +0000

    feat(class-exercise-5): Add a new TicketMachine
    - Added print statement to check user balance

--- 5a - Classes/Ticket-Machine/src/Driver.java
+++ 5a - Classes/Ticket-Machine/src/Driver.java
@@ -4,19 +4,21 @@ public class Driver {
         TicketMachine ticketDublin4 = new TicketMachine(300);
         TicketMachine ticketDublin1 = new TicketMachine(325);
         TicketMachine ticketDublin29 = new TicketMachine(150);
+        // create a new ticket machine
+        TicketMachine ticketMaynooth = new TicketMachine(220);
 
-        System.out.println(ticketDublin4.getPrice());
-        System.out.println(ticketDublin1.getPrice());
-        System.out.println(ticketDublin29.getPrice());
+        // print the price of a Maynooth ticket
+        System.out.println(ticketMaynooth.getPrice());
 
         // insert to little
-        ticketDublin1.insertMoney(100);
-        ticketDublin1.insertMoney(80);
-        System.out.println(ticketDublin1.getBalance());
-        ticketDublin1.insertMoney(145);
-        ticketDublin1.printTicket(); // ticket prints despite insufficent funds
-
-        // exercise 2 - print machine balance
-        System.out.println(ticketDublin1.getTotal());
+        ticketMaynooth.insertMoney(100);
+        ticketMaynooth.insertMoney(80);
+        System.out.println(ticketMaynooth.getBalance());
+        ticketMaynooth.insertMoney(40);
+        ticketMaynooth.printTicket();
+        // print user balance
+        System.out.println(ticketMaynooth.getBalance());
+        // from exercise 2 - print machine balance
+        System.out.println(ticketMaynooth.getTotal());
     }
 }
