1db7ff88e5cedb1b2f8b0ebf88ec8783ee99a50d
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-06 22:16:18 +0000

    feat(02-main): Add array of CoffeeCup instances

--- 5b - More Classes/CoffeeExample/src/Driver.java
+++ 5b - More Classes/CoffeeExample/src/Driver.java
@@ -1,12 +1,13 @@
 public class Driver {
     public static void main(String[] args) {
-        // Create an instance of CoffeeCup class
-        CoffeeCup myCup = new CoffeeCup("Latte", 350);
-        // Fill the cup
-        myCup.fill();
-        // Drink from the cup
-        myCup.drink(100);
-        // Output the remaining amount in the cup
-        System.out.println("Coffee left: " + myCup.checkLevel() + "ml");
+        // Create an array to hold CoffeeCup objects.
+        CoffeeCup[] cups = new CoffeeCup[5];
+        
+        // Create CoffeeCup instances.
+        cups[0] = new CoffeeCup("Latte", 350);
+        cups[1] = new CoffeeCup("Espresso", 100);
+        cups[2] = new CoffeeCup("Cappuccino", 250);
+        cups[4] = new CoffeeCup("Americano", 300);
+        cups[5] = new CoffeeCup("Mocha", 400);
     }
 }
