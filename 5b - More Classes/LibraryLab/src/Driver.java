2406b7e740a063c788d58fe43644b3164b060b9f
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-07 12:57:57 +0000

    feat(exercise-1.6): Create a Driver class to test the Book class with

---
+++ 5b - More Classes/LibraryLab/src/Driver.java
@@ -0,0 +1,13 @@
+/**
+ * This is the entry point to the program.
+ *
+ * <p>This contains the main method where the Book class will be tested.</p>
+ *
+ * @author Adam McCarthy
+ * @version 1.0
+ */
+public class Driver {
+    public static void main(String[] args) {
+    
+    }
+}
