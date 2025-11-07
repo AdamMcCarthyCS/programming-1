8894ed3457436d2b3caa7f260493e3b1f1c1f90f
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-07 12:12:28 +0000

    feat(display-method): Add method to return details of account

--- 5b - More Classes/BankAccountV4.0/src/BankAccount.java
+++ 5b - More Classes/BankAccountV4.0/src/BankAccount.java
@@ -4,11 +4,17 @@ public class BankAccount {
     double balance;
     
     public BankAccount() {
+    
     }
+    
     public BankAccount(String nm, String actNumber, double bal){
         name = nm;
         accountNumber = actNumber;
         balance = bal;
+    }
+    
+    public String display() {
+        return "Name: " + name + ", Account Number: " + accountNumber + ", Balance: €" + balance;
     }
     
 }
