ffa2ad2a5b49f1e68f30cbd7ef176e7b86bd2a5a
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-07 12:09:54 +0000

    chore(03-class): Create BankAccount class and paste code inside

---
+++ 5b - More Classes/BankAccountV4.0/src/BankAccount.java
@@ -0,0 +1,17 @@
+public class BankAccount {
+    String name;
+    String accountNumber;
+    double balance;
+    
+    public BankAccount() {
+    }
+    public BankAccount(String nm, String actNumber, double bal){
+        name = nm;
+        accountNumber = actNumber;
+        balance = bal;
+    }
+    
+}
+
+
+
