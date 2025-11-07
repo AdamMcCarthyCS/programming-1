3bd4e634bc0caa2dba81242f5371f54634900a5f
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-07 12:28:17 +0000

    docs(javadoc): Add initial javadocs to class and constructor

--- 5b - More Classes/LibraryLab/src/Book.java
+++ 5b - More Classes/LibraryLab/src/Book.java
@@ -1,10 +1,21 @@
+/**
+ * A class to represent a book.
+ *
+ * @author Adam McCarthy
+ * @version 1.0
+ */
 public class Book {
     // Fields (attributes)
     private String title;
     private String author;
     private int pages;
     
-    // Constructor
+    /**
+     * Constructor of Book class
+     *
+     * <p>The constructor sets the title, author, and number of pages of a book instance to
+     * default values</p>
+     */
     public Book() {
         title = "Unknown";
         author = "Unknown";
