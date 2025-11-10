d15863f0dca1532c1d3f9418c24e951ace51842a
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-10 22:06:19 +0000

    feat(printed-tests): Add updates to account and printed tests in Main class

--- 6a - Encapsulation/BankAccountV8.0/src/Main.java
+++ 6a - Encapsulation/BankAccountV8.0/src/Main.java
@@ -10,6 +10,40 @@ public class Main {
         System.out.println(withdrawMessage);
 //        bankAccount1.withdraw(200); // Without withdraw message
         
+        // Use the getters to print the bank account owner
+        System.out.println("The owner of this bank account is: " + bankAccount1.getName());
+        
+        // Use the setter to change the bank account owner
+        bankAccount1.setName("Adam McCarthy");
+        
+        // Print new value
+        System.out.println("The updated owner of this band account is: " + bankAccount1.getName());
+        
+        // My own changes:
+        // ---------------
+        
+        // Print account number:
+        System.out.println("The account number for this bank account is: "
+            + bankAccount1.getAccountNumber());
+        
+        // Change account number:
+        bankAccount1.setAccountNumber("9876543210");
+        
+        // Print updated account number
+        System.out.println("The updated account number for this bank account is: "
+            + bankAccount1.getAccountNumber());
+        
+        // Print account balance
+        System.out.println("The balance of this bank account is: €" + bankAccount1.getBalance());
+        
+        // Change the account balance
+        bankAccount1.setBalance(0);
+        
+        // print updated account balance
+        System.out.println("The updated balance of this bank account is: €"
+            + bankAccount1.getBalance());
+        
+        // print all account details
         System.out.println(bankAccount1.display());   // this  prints out the details to the console
     }
 }
