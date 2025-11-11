a840fabae806041eb8ba10ab84579e95214501a0
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-11 12:33:20 +0000

    feat(challenge): Add validation so name cannot be above length of 10 chars

---
+++ 6a - Encapsulation/lab6a_challenge_1/src/BankAccount.java
@@ -0,0 +1,89 @@
+public class BankAccount {
+    //-----------//
+    //  fields   //
+    //-----------//
+    private String name;
+    private String accountNumber;
+    private double balance;
+    
+    //---------------//
+    //  constructors //
+    //---------------//
+    
+    public BankAccount() {
+    }
+    public BankAccount(String name, String accountNumber, double balance){
+        if (name.length() <= 10) {
+            this.name = name;
+        } else {
+            this.name = name.substring(0, 10);
+        }
+        
+        this.accountNumber = accountNumber;
+        if (balance >= 0 && balance <= 1000) {
+            this.balance = balance;
+        } else {
+            this.balance = 50;
+        }
+    }
+    
+    //---------------//
+    //     methods   //
+    //---------------//
+    
+    public void deposit(double lodgement) {
+        balance += lodgement;
+    }
+    
+    public String withdraw(double withdrawl) {
+        if (withdrawl <= balance) {
+            balance -= withdrawl;
+            return "Withdrawl successful";
+        } else {
+            return "Unable to withdraw €" + withdrawl + " for " + name + " due to insufficient "
+                + "funds.";
+        }
+    }
+    
+    public String display(){
+        return "Name: " + name + ", Account Number: " + accountNumber + ", Balance: €" + balance;
+    }
+    
+    //---------------//
+    //    getters    //
+    //---------------//
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    //---------------//
+    //    setters    //
+    //---------------//
+    
+    public void setName(String name) {
+        if (name.length() <= 10) {
+            this.name = name;
+        }
+    }
+    
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    
+    public void setBalance(double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        }
+    }
+    
+}
+
