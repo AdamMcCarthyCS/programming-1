d826e533ec17c291435eb4987ee64ba5f2a2b2e4
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-12 21:28:55 +0000

    feat(driver-class): Add method to create a product

--- 6b - Encapsulation continued/ShopV1.0/src/Driver.java
+++ 6b - Encapsulation continued/ShopV1.0/src/Driver.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+/**
+ * This is where the program starts. It handles all user input and the output to the user
+ *
+ * @author Adam McCarthy
+ * @version 1.0
+ */
 public class Driver {
+    /**
+     * A Scanner instance to read in user input.
+     */
+    private Scanner input = new Scanner(System.in);
+    /**
+     * A Product instance which will be updated by user input.
+     */
+    private Product product;
+    
+    /**
+     * This is the entry point to the program.
+     *
+     * @param args  array of string arguments from command line
+     */
     public static void main(String[] args) {
     
+    }
+    
+    public void addProduct() {
+        // Read in the product name, code, and unit cost from the user.
+        System.out.print("Enter the product name: ");
+        String productName = input.nextLine();
+        System.out.print("Enter the Product code: ");
+        int productCode = input.nextInt();
+        System.out.print("Enter the item unit price: €");
+        double unitCost = input.nextDouble();
+        
+        //Ask the user to type in either a Y or an N.  This is then
+        //converted to either a True or a False (i.e. a boolean value).
+        System.out.print("Is this product in your current line (y/n): ");
+        char currentProduct = input.next().charAt(0);
+        boolean inCurrentProductLine = false;
+        if ( Character.toLowerCase(currentProduct) == 'c') {
+            inCurrentProductLine = true;
+        }
+        
+        product = new Product(productName, productCode, unitCost, inCurrentProductLine);
     }
 }
