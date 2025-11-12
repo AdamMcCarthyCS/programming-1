3c35c218128d3876b4649621a67f79a33f15f797
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-12 14:29:22 +0000

    feat(product-constructor): Add fields and constructor to initialise Product class

--- 6b - Encapsulation continued/ShopV1.0/src/Product.java
+++ 6b - Encapsulation continued/ShopV1.0/src/Product.java
@@ -1,2 +1,14 @@
 public class Product {
+    private String productName;
+    private int productCode;
+    private double unitCost;
+    private boolean isCurrentProductLine;
+    
+    public Product(String productName, int productCode, double unitCost,
+       boolean isCurrentProductLine) {
+        this.productName = productName;
+        this.productCode = productCode;
+        this.unitCost = unitCost;
+        this.isCurrentProductLine = isCurrentProductLine;
+    }
 }
