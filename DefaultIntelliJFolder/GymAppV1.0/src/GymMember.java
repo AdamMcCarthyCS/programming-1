37575879d5052874482bd625b03decdc9e4ab415
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-13 21:46:44 +0000

    feat(GymMember-fields): Add name, height, weight, membershipNumber, and currentGymMember fields
    - Add Javadoc to each field which can be generated with -private keyword

--- DefaultIntelliJFolder/GymAppV1.0/src/GymMember.java
+++ DefaultIntelliJFolder/GymAppV1.0/src/GymMember.java
@@ -2,11 +2,31 @@
  * This is a class that represents a gym member.
  *
  * <p>It has the fields: name, height, weight, membership number, and if the person is currently
- * a member. It also has public api to retrieve and set these fields and print a string
+ * a member. It also has public methods to retrieve and set these fields and print a string
  * representation.</p>
  *
  * @author      Adam McCarthy
  * @version     1.0
  */
 public class GymMember {
+    /**
+     * The gym members full name.
+     */
+    private String name = "";
+    /**
+     * The gym members weight in kilograms.
+     */
+    private double height = 0.5d;
+    /**
+     * The gym members height in meters.
+     */
+    private double weight = 25d;
+    /**
+     * The gym members membership number.
+     */
+    private int memberShipNumber = 99999;
+    /**
+     * Whether the person is currently a gym member, represented by true or false.
+     */
+    private boolean currentGymMember = false;
 }
