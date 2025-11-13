1670e9802af6dd35646a8a1952b32d19c011a081
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-13 21:28:09 +0000

    chore(challenge): Create GymMember and Driver class adding initial Javadocs

---
+++ DefaultIntelliJFolder/GymAppV1.0/src/Driver.java
@@ -0,0 +1,18 @@
+/**
+ * This is the entry point to the program. It controls all user input and output.
+ *
+ * @author      Adam McCarthy
+ * @version     1.0
+ * @see         GymMember
+ */
+public class Driver {
+    
+    /**
+     * Program starts here.
+     *
+     * @param args      a {@code String} array of command line arguments
+     */
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ DefaultIntelliJFolder/GymAppV1.0/src/GymMember.java
@@ -0,0 +1,12 @@
+/**
+ * This is a class that represents a gym member.
+ *
+ * <p>It has the fields: name, height, weight, membership number, and if the person is currently
+ * a member. It also has public api to retrieve and set these fields and print a string
+ * representation.</p>
+ *
+ * @author      Adam McCarthy
+ * @version     1.0
+ */
+public class GymMember {
+}
