b14be181aa1efb71bd0e2f62590c08500aa8ead9
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-27 10:52:50 +0000

    chore(shop-v2.2): Add starter files for exercise

---
+++ 7a - ArrayList/ShopV2.2/src/Driver.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+
+/**
+ * This class runs the application and handles the Product I/O
+ *
+ * @author Siobhan Drohan, Mairead Meagher
+ * @version 1.0
+ *
+ */
+public class Driver{
+
+    private Scanner input = new Scanner(System.in);
+    private Store store;
+
+    /**
+     * The main method is the starting point for the program.
+     * When started, the main method calls the addProduct() method
+     * followed by the printProduct() method.
+     *
+     * @param args  Any arguments for the main method
+     */
+    public static void main(String[] args) {
+		Driver driver = new Driver();
+		driver.processOrder();
+		driver.printProduct();
+        driver.printAverageProductPrice();
+        driver.printCheapestProduct();
+        System.out.print("View the products costing more than this price: ");
+        double price = driver.input.nextDouble();
+        driver.printProductsAbovePrice(price);
+	}
+
+    //gather the product data from the user and create a new product object.
+    private void addProduct(){
+    	System.out.print("Enter the Product Name:  ");
+        String productName = input.nextLine();
+        System.out.print("Enter the Product Code:  ");
+        int productCode = input.nextInt();
+        System.out.print("Enter the Unit Cost:  ");
+        double unitCost = input.nextDouble();
+        //Ask the user to type in either a Y or an N.  This is then
+        //converted to either a True or a False (i.e. a boolean value).
+    	System.out.print("Is this product in your current line (y/n): ");
+    	char currentProduct = input.next().charAt(0);
+    	boolean inCurrentProductLine = false;
+    	if ((currentProduct == 'y') || (currentProduct == 'Y'))
+    		inCurrentProductLine = true;
+    	
+        boolean isAdded = store.add(new Product(productName, productCode, unitCost, inCurrentProductLine));
+        if (isAdded) {
+            System.out.println("Product Added Successfully");
+        } else {
+            System.out.println("No product added");
+        }
+        input.nextLine();
+    }
+
+    public void processOrder() {
+        store = new Store();
+        System.out.print("How many products would you like to have in your store? ");
+        int numberOfProducts = input.nextInt();
+        input.nextLine();
+        for (int i = 0; i < numberOfProducts; i++) {
+            addProduct();
+            System.out.println();
+        }
+
+    }
+
+    /**
+     * Prints all products in the store.
+     */
+    private void printProduct(){
+    	System.out.println(store.listProducts());
+    }
+
+    /**
+     * Prints the average product price in the store
+     */
+    private void printAverageProductPrice() {
+        double averagePrice = store.averageProductPrice();
+        if (averagePrice == - 1) {
+            System.out.println("There are no products in the store.");
+        } else {
+            System.out.println("The average product price is: " + averagePrice);
+        }
+    }
+
+    /**
+     * Prints the name of the cheapest product in the store.
+     */
+    private void printCheapestProduct(){
+        Product cheapestProduct = store.cheapestProduct();
+        if (cheapestProduct == null) {
+            System.out.println("There are no products in the store.");
+        } else {
+            System.out.println("The cheapest product is: " + cheapestProduct.getProductName() + ".");
+        }
+    }
+
+    private void printProductsAbovePrice(double price) {
+        System.out.println(store.listProductsAbovePrice(price));
+    }
+
+
+
+}
