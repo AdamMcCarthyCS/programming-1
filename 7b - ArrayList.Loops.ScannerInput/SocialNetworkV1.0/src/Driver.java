cea89b0867ba20217f1b93410e798a3922f4fe52
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-29 13:51:42 +0000

    feat(Driver): Add main method and Javadocs

--- 7b - ArrayList.Loops.ScannerInput/SocialNetworkV1.0/src/Driver.java
+++ 7b - ArrayList.Loops.ScannerInput/SocialNetworkV1.0/src/Driver.java
@@ -1,2 +1,16 @@
+/**
+ * This class runs the application and handles user input/output.
+ *
+ * @author Adam McCarthy
+ * @version 1.0
+ */
 public class Driver {
+    /**
+     * The main method is the starting point for the program.
+     *
+     * @param args      a string array of command line arguments (unused)
+     */
+    public static void main(String[] args) {
+
+    }
 }
