b802d5f5decab30f2fa0926fba36011b150381e2
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-29 13:23:11 +0000

    feat(NewsFeed): Add posts ArrayList and constructor

--- 7b - ArrayList.Loops.ScannerInput/SocialNetworkV1.0/src/NewsFeed.java
+++ 7b - ArrayList.Loops.ScannerInput/SocialNetworkV1.0/src/NewsFeed.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * This class stores and returns posts to the user for printing.
+ *
+ * @author Adam McCarthy
+ * @version 1.0
+ */
 public class NewsFeed {
+    /**
+     * A collection of all posts in the newsfeed.
+     */
+    private ArrayList<MessagePost> posts;
+
+    /**
+     * Constructs a newsfeed object.
+     */
+    public NewsFeed(ArrayList<MessagePost> posts) {
+        this.posts = posts;
+    }
 }
