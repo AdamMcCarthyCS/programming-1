aa5b9407fa2e6ef4a562b0dabb1c9acc52aa2831
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-29 14:06:38 +0000

    feat(Driver): Create method addPost for storing messages

--- 7b - ArrayList.Loops.ScannerInput/SocialNetworkV1.0/src/Driver.java
+++ 7b - ArrayList.Loops.ScannerInput/SocialNetworkV1.0/src/Driver.java
@@ -5,12 +5,33 @@
  * @version 1.0
  */
 public class Driver {
+    private MessagePost post;
+    private NewsFeed newsfeed;
     /**
      * The main method is the starting point for the program.
      *
      * @param args      a string array of command line arguments (unused)
      */
     public static void main(String[] args) {
+        new Driver();
+    }
 
+    /**
+     * Reads user input to create a post and stores in newsfeed.
+     */
+    private void addPost() {
+        // Read in message details
+        String author = ScannerInput.readNextLine("Enter the message author: ");
+        String message = ScannerInput.readNextLine("Enter the message: ");
+        int likes = ScannerInput.readNextInt("Enter the number of likes received: ");
+
+        // Create a MessagePost instance
+        post = new MessagePost(author, message);
+        post.setLikes(likes);
+
+        // Add post to newsfeed
+        newsfeed.addPost(post);
+        // Add newline for formatting display for easier reading
+        System.out.println();
     }
 }
