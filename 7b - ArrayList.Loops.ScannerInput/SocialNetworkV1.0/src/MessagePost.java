fae7cc78127fda628483e1f891d59b218abb6d17
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-11-29 12:22:49 +0000

    feat(fields, constructor): Add author, message, likes fields and class constructor

--- 7b - ArrayList.Loops.ScannerInput/SocialNetworkV1.0/src/MessagePost.java
+++ 7b - ArrayList.Loops.ScannerInput/SocialNetworkV1.0/src/MessagePost.java
@@ -1,2 +1,19 @@
 public class MessagePost {
+    /**
+     * The message author
+     */
+    private String author = "";
+    /**
+     * The message content
+     */
+    private String message = "";
+    /**
+     * The number of likes the message received
+     */
+    private int likes = 0;
+
+    public MessagePost(String author, String message) {
+        this.author = author;
+        this.message = message;
+    }
 }
