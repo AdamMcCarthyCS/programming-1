853b75190d25f56d49c1c2216895df0f4e26b663
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-12-09 14:08:03 +0000

    feat(LikedPost): Add class LikedPost

---
+++ 9a - Inheritance (is-a) relationship/SocialNetworkV7.0/src/models/LikedPost.java
@@ -0,0 +1,13 @@
+package models;
+
+/**
+ * A parent class implementing some shared functionality of MessagePost and PhotoPost.
+ *
+ * @author Adam McCarthy
+ * @version 1.0 (09-Dec-25)
+ * @see Post
+ * @see MessagePost
+ * @see PhotoPost
+ */
+public class LikedPost {
+}
